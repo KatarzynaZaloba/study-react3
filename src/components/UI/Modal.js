43eba0b3beca3d5ce8be75cc205db5e9d6a1a8ad
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-11-13 18:30:36 +0100

    added Modal.module.css and added some code in Modal.js

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -14,10 +14,16 @@ const ModalOverlay = (props) => {
 };
 const portalElement = document.getElementById("root");
 
-const Modal = () => {
+const Modal = (props) => {
     return (
         <Fragment>
-            {ReactDOM.createPortal(<BackModal />)}
+            {ReactDOM.createPortal(<BackModal />,portalElement)}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
         </Fragment>
     );
 };
+
+export default Modal;
